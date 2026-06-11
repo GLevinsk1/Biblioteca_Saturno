0da016513b3c050769522920b8b43c25f8a98e88
G.Levinsk1 <gabriellevinski3@gmail.com>
2026-06-11 17:00:09 -0300

    Atualização Front end
    correção de erros visuais e melhoria na seleção de formato de livro

--- Front_end/script.js
+++ Front_end/script.js
@@ -11,19 +11,14 @@ const btn_fechar = document.getElementById("btn_fechar");
 const input_titulo = document.getElementById("input_titulo");
 const input_autor = document.getElementById("input_autor");
 const input_genero = document.getElementById("input_genero");
-
-const formato_kindle = document.getElementById("formato_kindle");
-const formato_fisico = document.getElementById("formato_fisico");
-const formato_audiobook = document.getElementById("formato_audiobook");
+const formato_livro = document.getElementById("formato_livro");
 
 btn_fechar.addEventListener("click", function () {
   if (
     input_titulo.value !== "" ||
     input_autor.value !== "" ||
     input_genero.value !== "" ||
-    formato_kindle.checked ||
-    formato_fisico.checked ||
-    formato_audiobook.checked
+    formato_livro.value !== ""
   ) {
     if (window.confirm("voce pode ter dados não salvos deseja fechar ?")) {
       div_cadastrar_livro.style.display = "none";
@@ -31,12 +26,37 @@ btn_fechar.addEventListener("click", function () {
       input_titulo.value = "";
       input_autor.value = "";
       input_genero.value = "";
-
-      formato_kindle.checked = false;
-      formato_fisico.checked = false;
-      formato_audiobook.checked = false;
+      formato_livro.value = "";
     }
   } else {
     div_cadastrar_livro.style.display = "none";
   }
 });
+<<<<<<< Updated upstream
+=======
+
+//atualizar botão de cadastro para capa do livro
+const input_capa = document.getElementById("input_capa");
+const btn_cadastrar_capa = document.getElementById("btn_cadastrar_capa");
+const arquivo_escolhido = document.getElementById("arquivo_escolhido");
+const capa_preview = document.getElementById("preview_capa");
+const leitor = new FileReader();
+
+input_capa.addEventListener("change", function () {
+  if (input_capa.files.length > 0) {
+    const arquivo = input_capa.files[0];
+
+    arquivo_escolhido.textContent = arquivo.name;
+
+    leitor.onload = function (e) {
+      capa_preview.src = e.target.result;
+    };
+    leitor.readAsDataURL(arquivo);
+    capa_preview.style.display = "block";
+  } else {
+    arquivo_escolhido.textContent = "nenhum arquivo";
+    capa_preview.src = "";
+    capa_preview.style.display = "none";
+  }
+});
+>>>>>>> Stashed changes
